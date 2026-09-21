4b5ef6ae679b9c6f3e9e01595e02f9f132697457
Yagub123 <gubz2006@gmail.com>
2026-09-21 11:06:54 -0400

    Update lab1b.py

--- lab1b.py
+++ lab1b.py
@@ -10,6 +10,19 @@
 #	Create a variable called "num1", take its value from user.
 #	Create another variable called "num2" and take its value from user. 
 # Convert the values to integers using int() function
-
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second mumber: "))
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
+#addition 
+print("Addition:",num1+num2)
+#subtraction
+print("Subtraction:",num1-num2)
+# Multiplication
+print("Multiplication:", num1 * num2)
+# Division
+print("Division:", num1 / num2)
+# Modulus (remainder)
+print("Modulus:", num1 % num2)
+# Exponentiation
+print("Exponent:", num1 ** num2)
